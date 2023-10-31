ae024e9ff1670fc03be162da408c994d38ec3ee3
muzzu <muzzueducation@gmail.com>
2023-10-31 22:18:28 +0530

    day one

---
+++ project 1/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+}
+
